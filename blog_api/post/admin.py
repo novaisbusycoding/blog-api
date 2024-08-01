b48ea7e4b1b579858cbca0bbf2312c2dd56062fd
Nova <novaisbusycoding@gmail.com>
2024-08-01 01:10:19 -0300

    feat: add post app

---
+++ blog_api/post/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post, Tag
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_at')
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
---
+++ blog_api/post/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils.text import slugify
+from ckeditor.fields import RichTextField
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+    content = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    tags = models.ManyToManyField(Tag, related_name='blog_posts', blank=True)
+    views = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):    
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
